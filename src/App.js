bfe3c8caad633d08b739095f12f03db35bba7268
Rincon Carranza <alan.rincon@accenture.com>
2023-06-05 12:26:40 -0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Header() {
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  return <Header/>;
+}
+
+export default App;
+
+// Investigar estandares de organización de proyectos de React.
+// Subirlo a Github.
+// Atomicidad, granualidad. << Formación de los componentes.
+// Clase abstracta poo react.
+// Formulario que refleje el contenido registrado.
+// Que sea reactivo, hookforms.
